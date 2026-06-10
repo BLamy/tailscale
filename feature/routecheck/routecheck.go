bbafda547341b5d13dace0f0fc722f52bbf8fafa
Simon Law <sfllaw@tailscale.com>
2026-06-10 02:33:16 -0700

    ipn/ipnext: add ipnext.NotifyWatcher for subscribing to the IPN bus
    Add a new ipnext.NotifyWatcher interface that exposes
    ipn.LocalBackend.WatchNotifications so that extensions inside
    tailscaled can subscribe to the IPN bus, much like how the GUI
    clients subscribe to it through the Local API.
    
    This interface will be used by feature/routecheck to watch for changes
    in the peer map that affect routers. See #17366.
    
    Updates #17366
    Updates #20062
    Updates tailscale/corp#33033
    
    Co-authored-by: Brad Fitzpatrick <bradfitz@tailscale.com>
    Signed-off-by: Simon Law <sfllaw@tailscale.com>

--- feature/routecheck/routecheck.go
+++ feature/routecheck/routecheck.go
@@ -13,12 +13,14 @@
 package routecheck
 
 import (
+	"context"
+	"errors"
 	"fmt"
 
 	"tailscale.com/ipn/ipnext"
 	"tailscale.com/net/routecheck"
+	"tailscale.com/tailcfg"
 	"tailscale.com/types/logger"
-	"tailscale.com/types/netmap"
 )
 
 // FeatureName is the name of the feature implemented by this package.
@@ -42,6 +44,7 @@ type Extension struct {
 	backend ipnext.SafeBackend
 	nb      nodeBackender
 	nm      routecheck.NetMapper
+	routers *RouterTracker
 }
 
 var _ ipnext.Extension = new(Extension)
@@ -53,6 +56,8 @@ func (e *Extension) Name() string {
 
 // Init implements the [ipnext.Extension.Init] interface method.
 func (e *Extension) Init(h ipnext.Host) error {
+	ctx := context.Background()
+
 	e.nb = nodeBackender{h}
 
 	nm, ok := e.backend.(routecheck.NetMapper)
@@ -60,6 +65,11 @@ func (e *Extension) Init(h ipnext.Host) error {
 		return fmt.Errorf("backend %T does not implement routecheck.NetMapper", e.backend)
 	}
 	e.nm = nm
+
+	ipnbus, ok := e.backend.(ipnext.NotifyWatcher)
+	if !ok {
+		return fmt.Errorf("backend %T does not implement ipnext.NotifyWatcher", e.backend)
+	}
 
 	pinger := e.backend.Sys().Engine.Get()
 
@@ -69,20 +79,52 @@ func (e *Extension) Init(h ipnext.Host) error {
 	}
 	e.Client = c
 
-	h.Hooks().OnNetMapToggle.Add(e.onNetMapToggle)
+	e.routers = TrackRouters(ctx, e.logf, ipnbus)
+	e.routers.OnNetMapAvailable = e.Client.NotifyNetMapAvailable
+	e.routers.OnRoutersChange = e.onRoutersChange
+
+	h.Hooks().OnSelfChange.Add(e.onSelfChange)
+
+	// Unlike a cold start, starting with a cached netmap
+	// may have pre-loaded a valid NodeBackend.Self,
+	// so an initial OnSelfChange won’t fire and we have to do it ourselves.
+	// If the cached netmap was running the watcher goroutine, this will restart it.
+	if self := e.nb.NodeBackend().Self(); self.Valid() {
+		e.onSelfChange(self)
+	}
 
 	return nil
 }
 
 // Shutdown implements the [ipnext.Extension.Shutdown] interface method.
 func (e *Extension) Shutdown() error {
-	err := e.Client.Close()
-	return err
+	e.routers.Close()
+	return e.Client.Close()
 }
 
-func (e *Extension) onNetMapToggle(nm *netmap.NetworkMap) {
-	if nm == nil {
-		return
-	}
-	e.Client.NotifyNetMapAvailable(nm)
+func (e *Extension) needsRefresh() {
+	// TODO(sfllaw): Call e.Client.NeedsRefresh() after implementing it.
+}
+
+func (e *Extension) onRoutersChange(added, modified, removed []tailcfg.NodeID) {
+	// TODO(sfllaw): This refresh could be incremental,
+	// based on the added, modified, and removed nodes.
+	e.needsRefresh()
+}
+
+func (e *Extension) onSelfChange(self tailcfg.NodeView) {
+	// onSelfChange must never block, because it’s called from
+	// [ipnlocal.LocalBackend.SetControlClientStatus], which locks
+	// LocalBackend.mu. This lock is also acquired when unwinding
+	// [ipnlocal.LocalBackend.WatchNotificationsAs] which is what
+	// [RouterTracker.stopWatcherLocked] is waiting for.
+	go func() {
+		if _, _, err := e.routers.StartStopWatcher(self); err != nil {
+			if !errors.Is(err, routecheckNotEnabledErr) {
+				e.logf("error tracking routers: %v", err)
+			}
+			return // can be started by toggling the nodeattr
+		}
+		e.needsRefresh()
+	}()
 }
